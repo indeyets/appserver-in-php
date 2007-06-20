a31a1be980c1d7555165be2e87b167c523a68969
indeyets <indeyets@6af6eaaa-4033-0410-ba69-1dabb174101c>
2007-06-20 21:29:07 +0000

    working ezGraph app
    git-svn-id: https://appserver-in-php.googlecode.com/svn/trunk@7 6af6eaaa-4033-0410-ba69-1dabb174101c

--- examples/scgi-ezc/GraphApp.class.php
+++ examples/scgi-ezc/GraphApp.class.php
@@ -4,6 +4,48 @@ class GraphApp
 {
     public static function main()
     {
-        echo "<h1>Hello, world!</h1>";
+        if (file_exists('result.svg'))
+            unlink('result.svg');
+
+        $mem1 = memory_get_usage();
+        $start = microtime(true);
+        self::createGraph();
+        $end = microtime(true);
+        $mem2 = memory_get_usage();
+
+        echo '<p>time for generation: '.($end - $start).' seconds</p>';
+        echo '<p>memory (before/after): '.$mem1.'/'.$mem2.'</p>';
+    }
+
+    public static function createGraph()
+    {
+        $graph = new ezcGraphPieChart();
+        $graph->palette = new ezcGraphPaletteEzRed();
+        $graph->title = 'Access statistics';
+        $graph->legend = false;
+
+        $graph->data['Access statistics'] = new ezcGraphArrayDataSet(array(
+            'Mozilla' => 19113,
+            'Explorer' => 10917,
+            'Opera' => 1464,
+            'Safari' => 652,
+            'Konqueror' => 474,
+        ));
+        $graph->data['Access statistics']->highlight['Explorer'] = true;
+
+        // $graph->renderer = new ezcGraphRenderer2d();
+
+        $graph->renderer->options->moveOut = .2;
+
+        $graph->renderer->options->pieChartOffset = 63;
+
+        $graph->renderer->options->pieChartGleam = .3;
+        $graph->renderer->options->pieChartGleamColor = '#FFFFFF';
+        $graph->renderer->options->pieChartGleamBorder = 2;
+
+        $graph->renderer->options->pieChartShadowSize = 5;
+        $graph->renderer->options->pieChartShadowColor = '#BABDB6';
+
+        $graph->render(400, 150, 'result.svg');
     }
 }
