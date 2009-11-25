27d64353f2c45255cd3734f32c375b21b2efaaa4
Alexey Zakhlestin <indeyets@gmail.com>
2009-11-25 23:09:10 +0300

    autoloader + exceptions definitions

---
+++ Middleware/Session/autoload.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace MFS\AppServer\Middleware\Session;
+
+function autoload($class_name)
+{
+    static $files = null;
+
+    if (null === $files) {
+        $root = __DIR__.'/';
+
+        $files = array(
+            'MFS\AppServer\Middleware\Session\Session'                  => $root.'PHP_Compat.class.php',
+            'MFS\AppServer\Middleware\Session\_Engine'                  => $root.'_Engine.class.php',
+
+            'MFS\AppServer\Middleware\Session\RuntimeException'         => $root.'exceptions.php',
+            'MFS\AppServer\Middleware\Session\LogicException'           => $root.'exceptions.php',
+            'MFS\AppServer\Middleware\Session\UnexpectedValueException' => $root.'exceptions.php',
+            'MFS\AppServer\Middleware\Session\OutOfBoundsException'     => $root.'exceptions.php',
+        );
+    }
+
+    if (isset($files[$class_name]))
+        require $files[$class_name];
+}
+
+spl_autoload_register('MFS\AppServer\Middleware\Session\autoload');
---
+++ Middleware/Session/exceptions.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MFS\AppServer\Middleware\Session;
+
+class RuntimeException extends \RuntimeException {}
+class LogicException extends \LogicException {}
+
+class UnexpectedValueException extends \UnexpectedValueException {}
+class OutOfBoundsException extends \OutOfBoundsException {}
