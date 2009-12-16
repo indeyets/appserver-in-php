f30ed6b72cb7023a73ef6604e44645280fa8f615
Alexey Zakhlestin <indeyets@gmail.com>
2009-12-16 21:30:19 +0300

    fix namespace

--- examples/ev/client.php
+++ examples/ev/client.php
@@ -2,8 +2,8 @@
 
 require __DIR__.'/../../autoload.php';
 
-$client = new \MFS\SCGI\Client('tcp://127.0.0.1:9999');
-$req = new \MFS\SCGI\ClientRequest($client);
+$client = new \MFS\AppServer\SCGI\Client('tcp://127.0.0.1:9999');
+$req = new \MFS\AppServer\SCGI\ClientRequest($client);
 
 $req->setURI('/?foo=42');
 $req->setMethod('POST');
