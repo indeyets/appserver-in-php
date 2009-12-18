bc92715a1c0019598593b894e2639f9319b95274
Alexey Zakhlestin <indeyets@gmail.com>
2009-12-18 16:10:09 +0300

    removed use of aliases. those might complicate understanding

--- examples/new/runner.php
+++ examples/new/runner.php
@@ -3,23 +3,16 @@
 require 'MyApp.class.php';
 require realpath(__DIR__.'/../..').'/autoload.php';
 
-use \MFS\AppServer\Middleware\PHP_Compat\PHP_Compat as php_compat;
-use \MFS\AppServer\Middleware\URLMap\URLMap as urlmap;
-use \MFS\AppServer\SCGI\Handler as scgi;
-use \MFS\AppServer\HTTP\Handler as http;
-use \MFS\AppServer\MOD_PHP\Handler as mod_php;
-
-
 // init application
 $app = new MyApp();
 
 if (PHP_SAPI === 'cli') {
     // app expects php-style vars. in SCGI-mode we need them using middleware
-    $app = new php_compat($app);
+    $app = new \MFS\AppServer\Middleware\PHP_Compat\PHP_Compat($app);
 }
 
 // serving hello-app on "/hello", other way serving $app
-$app = new urlmap(array(
+$app = new \MFS\AppServer\Middleware\URLMap\URLMap(array(
     '/hello' => function(){ return array(200, array('Content-type', 'text/plain'), 'Hello, world!'); },
     '/' => $app
 ));
@@ -27,13 +20,13 @@ $app = new urlmap(array(
 // choosing appropriate handler
 if (PHP_SAPI === 'cli') {
     if (isset($argv[1]) and $argv[1] === 'http') {
-        $handler = new http('tcp://127.0.0.1:8080');
+        $handler = new \MFS\AppServer\HTTP\Handler('tcp://127.0.0.1:8080');
     } else {
-        $handler = new scgi('tcp://127.0.0.1:9999');
+        $handler = new \MFS\AppServer\SCGI\Handler('tcp://127.0.0.1:9999');
     }
 } else {
     ini_set('display_errors', 'Off');
-    $handler = new mod_php();
+    $handler = new \MFS\AppServer\MOD_PHP\Handler();
 }
 
 // serving app
