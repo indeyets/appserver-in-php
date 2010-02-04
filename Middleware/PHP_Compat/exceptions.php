5cf092fb5bf508bf93d6cbd84a787c7f7f04e776
Alexey Zakhlestin <indeyets@gmail.com>
2010-02-04 21:02:44 +0300

    add missed local versions of exceptions

--- Middleware/PHP_Compat/exceptions.php
+++ Middleware/PHP_Compat/exceptions.php
@@ -2,6 +2,9 @@
 
 namespace MFS\AppServer\Middleware\PHP_Compat;
 
+class LogicException extends \LogicException {}
+
+class OutOfBoundsException extends \OutOfBoundsException {}
 class UnexpectedValueException extends \UnexpectedValueException {}
 class InvalidArgumentException extends \InvalidArgumentException {}
 
