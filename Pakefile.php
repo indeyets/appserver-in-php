ff315904a30d7532311edb457425466617b80418
Alexey Zakhlestin <indeyets@gmail.com>
2010-04-13 21:28:14 +0200

    show output

---
+++ Pakefile.php
@@ -0,0 +1,19 @@
+<?php
+
+pake_import('pear');
+
+pake_desc('package and install current snapshot');
+pake_task('install', 'pear_package');
+
+pake_desc('run Demo-application');
+pake_task('demo');
+
+function run_install()
+{
+    pake_superuser_sh('pear install -f AppServer_backport52-0.2.0.tgz');
+}
+
+function run_demo()
+{
+    pake_sh('aip app '.realpath(__DIR__.'/examples/new/config.yaml'), true);
+}
