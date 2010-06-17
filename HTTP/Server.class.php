bcb078b5b81a54479f7dc1857c4a4cceea1d678e
Alexey Zakhlestin <indeyets@gmail.com>
2010-06-17 12:33:48 +0400

    close stream, after getting data from it

--- HTTP/Server.class.php
+++ HTTP/Server.class.php
@@ -16,14 +16,20 @@ class Server implements \MFS\AppServer\iProtocol
             $response .= $response_data[1][$i].': '.$response_data[1][++$i]."\r\n";
         }
         $response .= "\r\n";
-        if (!is_resource($response_data[2]))
+
+        // reponse is string
+        if (is_string($response_data[2]))
             $response .= $response_data[2];
 
         $this->write($response); // body
 
-        if (is_resource($response_data[2]))
-            while (!feof($response_data[2]))
+        // response is stream
+        if (is_resource($response_data[2])) {
+            while (!feof($response_data[2])) {
                 $this->write(fread($response_data[2], 1024));
+            }
+            fclose($response_data[2]);
+        }
     }
 
     public function readRequest($stream)
