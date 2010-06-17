f70fc04c5d61d44c8a3e1a62f0c85225d21c4977
Alexey Zakhlestin <indeyets@gmail.com>
2010-06-17 12:34:09 +0400

    add stream-support to MOD_PHP server

--- MOD_PHP/Handler.class.php
+++ MOD_PHP/Handler.class.php
@@ -38,6 +38,7 @@ class MFS_AppServer_MOD_PHP_Handler implements MFS_AppServer_iHandler
 
             $result = call_user_func($app, $context);
 
+            // headers
             $response = new MFS_AppServer_MOD_PHP_Response();
             $response->setStatus($result[0]);
             for ($i = 0, $cnt = count($result[1]); $i < $cnt; $i++) {
@@ -45,7 +46,15 @@ class MFS_AppServer_MOD_PHP_Handler implements MFS_AppServer_iHandler
             }
             unset($response);
 
-            echo $result[2];
+            // body
+            if (is_string($result[2])) {
+                echo $result[2];
+            } elseif (is_resource($result[2])) {
+                while(!feof($result[2])) {
+                    $this->write(fread($result[2], 1024));
+                }
+                fclose($result[2]);
+            }
             unset($result);
 
             $this->log("-> done with request");
