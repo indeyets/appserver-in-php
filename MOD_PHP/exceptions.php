2a2e17714f0d2b3c7ef905ef3f316bb742b93df4
Alexey Zakhlestin <indeyets@gmail.com>
2010-12-08 23:47:14 +0300

    missed Exception in mod_php handler

--- MOD_PHP/exceptions.php
+++ MOD_PHP/exceptions.php
@@ -4,3 +4,4 @@ namespace MFS\AppServer\MOD_PHP;
 class Exception extends \Exception {}
 class LogicException extends \LogicException {}
 class RuntimeException extends \RuntimeException {}
+class InvalidArgumentException extends \InvalidArgumentException {}
