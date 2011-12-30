94b9b8314ddb4b28e01f99959543440f0c7ee64c
Henri Bergius <henri.bergius@iki.fi>
2011-12-30 16:15:39 +0200

    Quick-and-dirty smoketest for Travis

---
+++ tests/FileserverTest.php
@@ -0,0 +1,26 @@
+<?php
+class FileserverTest extends PHPUnit_Framework_TestCase
+{
+    private $process;
+    private $pipes = array();
+    public function setUp()
+    {
+        $path = dirname(__DIR__);
+        $pipes = array();
+        $this->process = proc_open("php {$path}/bin/aip files", array(), $this->pipes, $path);
+        sleep(2);
+    }
+
+    public function testLoadFile()
+    {
+        $config = file_get_contents('http://127.0.0.1:8080/composer.json');
+        $configParsed = json_decode($config);
+        $this->assertEquals('aip/aip', $configParsed->name);
+    }
+
+    public function tearDown()
+    {
+        proc_terminate($this->process);
+    }
+}
+
---
+++ tests/travis_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Install with Composer
+wget http://getcomposer.org/composer.phar 
+php composer.phar install
