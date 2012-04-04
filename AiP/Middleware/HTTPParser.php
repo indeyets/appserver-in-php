indeyets/appserver-in-php

commit 6a6310cf5387786c67a460649f83a2e6c2eab2c7

Don't reassign , PHP 5.4 doesn't like that

AiP/Middleware/HTTPParser.php
@@ -88,7 +88,7 @@ class HTTPParser
     }
 
 
-    private static function parseMultipart($ct, $b, &$_POST, &$_FILES)
+    private static function parseMultipart($ct, $b, &$POST, &$FILES)
     {
         $vars_accu = array(); // place to accumulate parts of post-vars string
 
@@ -225,7 +225,7 @@ class HTTPParser
             $pos = $b_end + 2;
         }
 
-        parse_str(implode('&', $post_strs), $_POST);
+        parse_str(implode('&', $post_strs), $POST);
     }
 
     // utility functions

commit a576a982f162930b284caec1be156f26152931fb

use proper var for files-upload handling (was broken by 6a6310c)

AiP/Middleware/HTTPParser.php
@@ -210,9 +210,9 @@ class HTTPParser
                 $parts = explode('[', $sel, 2);
                 foreach (array_keys($fdata) as $key) {
                     if (count($parts) == 1) {
-                        $_FILES[$disposition['name']][$key] = $fdata[$key];
+                        $FILES[$disposition['name']][$key] = $fdata[$key];
                     } else {
-                        eval($code = '$_FILES[' . $parts[0] . '][\'' . $key . '\'][' . $parts[1] . ' = $fdata[\'' . $key . '\'];');
+                        eval($code = '$FILES[' . $parts[0] . '][\'' . $key . '\'][' . $parts[1] . ' = $fdata[\'' . $key . '\'];');
                     }
                 }
             } else {