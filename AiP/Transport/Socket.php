ac7e060e6e223513d0ff36a2dc9dae28fb3f6222
Alexey Zakhlestin <indeyets@gmail.com>
2012-11-19 19:10:25 +0400

    refuse to work on php versions, which are known to be buggy

--- AiP/Transport/Socket.php
+++ AiP/Transport/Socket.php
@@ -12,6 +12,17 @@ class Socket extends AbstractTransport
 
     protected $in_loop = false;
 
+    public function __construct($addr, $callback)
+    {
+        if (PHP_MAJOR_VERSION === 5 and PHP_MINOR_VERSION === 3 and in_array(PHP_RELEASE_VERSION, array(9, 10))) {
+            throw new \LogicException('PHP 5.3.9 and 5.3.10 have bug in stream_get_line(). see https://bugs.php.net/bug.php?id=60817');
+        } elseif (PHP_MAJOR_VERSION === 5 and PHP_MINOR_VERSION === 4 and PHP_RELEASE_VERSION <= 3) {
+            throw new \LogicException('PHP versions earlier than 5.4.3 have bug in stream_get_line(). see https://bugs.php.net/bug.php?id=60817');
+        }
+
+        parent::__construct($addr, $callback);
+    }
+
     public function loop()
     {
         $this->in_loop = true;
